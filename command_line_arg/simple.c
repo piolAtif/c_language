5f1617e6554339ba3565876bf50cc670227daa15
spreeti <spreeti@INspreeti.local>
2016-02-03 15:34:15 +0530

    Modified text of simple.c

--- command_line_arg/simple.c
+++ command_line_arg/simple.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char const *argv[]){
 	if(argc == 2)
-		printf("pagalo apna kam karo %s",argv[1]);
+		printf("This is a server %s",argv[1]);
 	else if(argc>2)
-		printf("hamara neta kon hai%s\n",argv[2]);
+		printf("Hello world! my name is %s\n",argv[2]);
 	else
-		printf("baki bathein peene bad\n");
+		printf("Sorry! type some text\n");
 	return 0;
 
